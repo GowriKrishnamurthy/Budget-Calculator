6942c69f7ffa99ea6c08b5c4c23a554248771244
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-03-22 19:33:35 +1100

    Budget Calc

---
+++ app.js
@@ -0,0 +1,34 @@
+// 1. budgetController MODULE that handles budget 
+var budgetController = (function(){
+
+})();
+
+// 2. MODULE that handles User Interface 
+var UIController=(function(){
+    return {
+        getInput:function(){
+        document.getElementById("")
+        }
+    }
+    
+})();
+
+
+// 3. Data MODULE(GLOBAL APP CONTROLLER) that handles User Interface 
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var ctrlAddItem = function(){
+        // 1. Get the field input data.
+        // 2. Add the item to the budget controller.
+        // 3. Add the item to the UI.
+        // 4. Calcuate the budget.
+        // 5. Display  the budget on the UI.        
+    }
+    document.querySelector('.add__btn').addEventListener('click',ctrlAddItem);
+    
+    document.addEventListener('keypress', function (event) {
+    if (event.keyCode=== 13  || event.which=== 13 ) { // 13 is enter
+              ctrlAddItem();
+    }
+    });    
+})(budgetController,UIController);
